57d1dec225f38b17eca54bda1af413a8d1aae9d0
Tsuneo Nakagawa <yaemon.the.tank@gmail.com>
2018-07-12 02:34:59 +0900

    GameVersion 1.4.0 announced

--- js/checktool/check.sh
+++ js/checktool/check.sh
@@ -18,7 +18,7 @@ CHECK=`mktemp`
 
 sed -ne '/"n1": /p;/END_OF_NISHIN_GATTAI/q;' $1 > $DATA
 
-LIST="大天使 女神 霊鳥 魔神 神獣 聖獣 幻魔 破壊神 地母神 鬼神 天使 妖魔 龍王"
+LIST="大天使 女神 霊鳥 魔神 神獣 聖獣 幻魔 破壊神 龍神 地母神 鬼神 天使 妖魔 龍王"
 LIST=`echo $LIST " 魔獣 妖精 堕天使 妖鬼 鬼女 夜魔 邪神 妖獣 外道 魔王 幽鬼"`
 
 for TARGET in $LIST ; do
